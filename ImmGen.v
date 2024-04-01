5332fcd470e923c37a55e3a42fc1fd4cffc16925
Nadia Dessouky <149896918+nadiadessouky24@users.noreply.github.com>
2024-04-01 20:50:24 +0200

    Update ImmGen.v

--- ImmGen.v
+++ ImmGen.v
@@ -1,27 +1,21 @@
-`timescale 1ns / 1ps
+`include "defines.v"
 
+module rv32_ImmGen (
+    input  wire [31:0]  IR,
+    output reg  [31:0]  Imm
+);
 
-module ImmGen( input [31:0] inst, output reg [31:0] gen_out);
-
-
-always @(*)
-begin
-    if (inst[6] == 0)
-    begin
-        if (inst[5] == 0)
-        begin //lw 
-            gen_out = {{20{inst[31]}} , inst[31:20]};                                                                                                                                                               
-        
-        end
-        else if (inst[5] == 1) //sw
-        begin
-            gen_out = {{20{inst[31]}} , inst[31:25], inst[11:7]};
-        end
-    end    
-    else if (inst[6] == 1) //BEQ
-    begin
-        gen_out = {{20{inst[31]}} , inst[31], inst[7],inst[30:25],inst[11:8]};
-    end 
+always @(*) begin
+	case (`OPCODE)
+		`OPCODE_Arith_I   : 	Imm = { {21{IR[31]}}, IR[30:25], IR[24:21], IR[20] };
+		`OPCODE_Store     :     Imm = { {21{IR[31]}}, IR[30:25], IR[11:8], IR[7] };
+		`OPCODE_LUI       :     Imm = { IR[31], IR[30:20], IR[19:12], 12'b0 };
+		`OPCODE_AUIPC     :     Imm = { IR[31], IR[30:20], IR[19:12], 12'b0 };
+		`OPCODE_JAL       : 	Imm = { {12{IR[31]}}, IR[19:12], IR[20], IR[30:25], IR[24:21], 1'b0 };
+		`OPCODE_JALR      : 	Imm = { {21{IR[31]}}, IR[30:25], IR[24:21], IR[20] };
+		`OPCODE_Branch    : 	Imm = { {20{IR[31]}}, IR[7], IR[30:25], IR[11:8], 1'b0};
+		default           : 	Imm = { {21{IR[31]}}, IR[30:25], IR[24:21], IR[20] }; // IMM_I
+	endcase 
 end
 
 endmodule
